13c84c0dbbfa7020b59d2118ba190dc84c8c8a4e
AvAarsNS <arthur.vanaarssen@ns.nl>
2022-05-16 14:39:04 +0200

    First red

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
+function createBoard() {
+
+}
 
 module.exports = {
+    createBoard
 }
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,1 +1,8 @@
-const {} = require('../src/functions');
+const {createBoard} = require('../src/functions');
+
+describe('We start with an empty board', () => {
+    it('We should be able to create... ', () => {
+        const board = createBoard();
+        expect(board).toBe([[null,null,null],[null,null,null],[null,null,null]])
+    });
+});
