8769c9191248700cd84710ee801a989048884a58
AvAarsNS <arthur.vanaarssen@ns.nl>
2022-05-16 14:40:17 +0200

    Make first test green!

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,7 @@
 function createBoard() {
-
+    return [[null, null, null],
+            [null, null, null],
+            [null, null, null]];
 }
 
 module.exports = {
--- test/functions.test.js
+++ test/functions.test.js
@@ -3,6 +3,6 @@ const {createBoard} = require('../src/functions');
 describe('We start with an empty board', () => {
     it('We should be able to create... ', () => {
         const board = createBoard();
-        expect(board).toBe([[null,null,null],[null,null,null],[null,null,null]])
+        expect(board).toStrictEqual([[null,null,null],[null,null,null],[null,null,null]])
     });
 });
